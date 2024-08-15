857f2d828596aaf89da35073229319e67b0eae39
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-08-15 19:10:32 +0100

    columns fit tasks

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -6,14 +6,14 @@ import TaskCard from "./TaskCard";
 export default function Column({ column, data, setData }) {
   return (
     <>
-      <Card className="min-w-[336px] min-h-[420px]">
+      <Card className="min-w-[336px] h-fit">
         <CardHeader>
           <CardTitle className="subpixel-antialiased text-ellipsis">
             {column.name}
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <ScrollArea className="min-h-[280px] min-w-[280px]">
+          <ScrollArea className="min-h-[88px] min-w-[280px]">
             <div className="flex flex-col gap-3">
               {column.tasks.map((task) => (
                 <TaskCard task={task} key={task.id} />
