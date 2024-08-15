3f48eb0975773effc5a7e288088bc9017679922a
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-08-15 15:51:52 +0100

    data structure for multiple boards (in lib)

---
+++ src/lib/data.ts
@@ -0,0 +1,274 @@
+const data = [
+  {
+    name: "programming-board",
+    id: "03df9erc",
+    columns: [
+      {
+        name: "todo",
+        id: "360d11ac",
+        tasks: [
+          {
+            name: "ImplementLoginFeature",
+            id: "8e31fc82",
+          },
+          {
+            name: "SetupDatabaseSchema",
+            id: "9e28a0e5",
+          },
+          {
+            name: "CreateUserInterfaceMockups",
+            id: "6b68b752",
+          },
+          {
+            name: "ConfigureCI/CDPipeline",
+            id: "68854bd1",
+          },
+          {
+            name: "WriteUnitTestsForAPI",
+            id: "5651d51d",
+          },
+        ],
+      },
+      {
+        name: "doing",
+        id: "4ec5e4f5",
+        tasks: [
+          {
+            name: "RefactorAuthenticationModule",
+            id: "daffddfb",
+          },
+          {
+            name: "OptimizeSQLQueries",
+            id: "eaca1174",
+          },
+          {
+            name: "IntegrateOAuth2.0",
+            id: "832a9dc9",
+          },
+          {
+            name: "DevelopRESTfulEndpoints",
+            id: "86fd8938",
+          },
+          {
+            name: "ImplementErrorHandling",
+            id: "2af1e579",
+          },
+        ],
+      },
+      {
+        name: "review",
+        id: "39c647f0",
+        tasks: [
+          {
+            name: "CodeReviewFeatureX",
+            id: "d6f8a1e9",
+          },
+          {
+            name: "TestAPIEndpoints",
+            id: "3d866750",
+          },
+          {
+            name: "ReviewDatabaseMigrations",
+            id: "233eb965",
+          },
+          {
+            name: "CheckCrossBrowserCompatibility",
+            id: "c5b49a44",
+          },
+          {
+            name: "AnalyzePerformanceMetrics",
+            id: "6f6ef78c",
+          },
+        ],
+      },
+      {
+        name: "done",
+        id: "13a35ce3",
+        tasks: [
+          {
+            name: "DeployToStagingEnvironment",
+            id: "4b040a5e",
+          },
+          {
+            name: "FixCriticalBugInProduction",
+            id: "3cbdbddd",
+          },
+          {
+            name: "CompleteCodeRefactoring",
+            id: "60d18e1b",
+          },
+          {
+            name: "DocumentAPIEndpoints",
+            id: "6bcaf70b",
+          },
+          {
+            name: "MergePullRequest",
+            id: "a6a4d9ee",
+          },
+        ],
+      },
+      {
+        name: "backlog",
+        id: "7847dad3",
+        tasks: [
+          {
+            name: "ResearchNewFramework",
+            id: "bbef7c0f",
+          },
+          {
+            name: "PlanFeatureYDevelopment",
+            id: "b8571f19",
+          },
+          {
+            name: "SetUpLoadTesting",
+            id: "b3b05a15",
+          },
+          {
+            name: "DesignDatabaseScalingStrategy",
+            id: "b1a949fb",
+          },
+          {
+            name: "InvestigateMemoryLeak",
+            id: "41e60090",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    name: "design-board",
+    id: "e0bf32a7",
+    columns: [
+      {
+        name: "todo",
+        id: "192d839c",
+        tasks: [
+          {
+            name: "CreateWireframes",
+            id: "b2a741a2",
+          },
+          {
+            name: "SelectColorScheme",
+            id: "f8b725c3",
+          },
+          {
+            name: "DefineTypography",
+            id: "f3a7e8b1",
+          },
+          {
+            name: "OutlineUserFlow",
+            id: "e05b2ad8",
+          },
+          {
+            name: "DevelopStyleGuide",
+            id: "d8e15f6b",
+          },
+        ],
+      },
+      {
+        name: "doing",
+        id: "9e5d38c2",
+        tasks: [
+          {
+            name: "DesignHomepage",
+            id: "2d957b7d",
+          },
+          {
+            name: "CreateIcons",
+            id: "6d3a5c18",
+          },
+          {
+            name: "DesignMobileVersion",
+            id: "cb5a9d4e",
+          },
+          {
+            name: "PrototypeUserDashboard",
+            id: "f64518c9",
+          },
+          {
+            name: "CreateResponsiveLayouts",
+            id: "f93b65cd",
+          },
+        ],
+      },
+      {
+        name: "review",
+        id: "8d1c9e7a",
+        tasks: [
+          {
+            name: "ReviewUserFeedback",
+            id: "b1a4e5a2",
+          },
+          {
+            name: "TestUIAccessibility",
+            id: "3f8a7d5c",
+          },
+          {
+            name: "FinalizeLogoDesign",
+            id: "5f2a7c9d",
+          },
+          {
+            name: "AnalyzeA/BTestingResults",
+            id: "7d4a9c3b",
+          },
+          {
+            name: "CheckPrototypeConsistency",
+            id: "f0e8b9a1",
+          },
+        ],
+      },
+      {
+        name: "done",
+        id: "fb9d4a38",
+        tasks: [
+          {
+            name: "LaunchMarketingSite",
+            id: "3e8a6d1f",
+          },
+          {
+            name: "FinalizeStyleGuide",
+            id: "a1b2e3c4",
+          },
+          {
+            name: "CompleteDesignSystem",
+            id: "e9b7a5c6",
+          },
+          {
+            name: "SubmitFinalDesigns",
+            id: "7c5e4f2d",
+          },
+          {
+            name: "PublishDesignAssets",
+            id: "6e3f4a8b",
+          },
+        ],
+      },
+      {
+        name: "backlog",
+        id: "98f4b1a9",
+        tasks: [
+          {
+            name: "ExploreNewDesignTrends",
+            id: "c8f2d7e5",
+          },
+          {
+            name: "ResearchUserPersonas",
+            id: "e7c9a6f3",
+          },
+          {
+            name: "PlanNextProject",
+            id: "d4e8c7a1",
+          },
+          {
+            name: "DesignHighFidelityMockups",
+            id: "b1f4e6a7",
+          },
+          {
+            name: "RefineUserExperience",
+            id: "c2d3f7a4",
+          },
+        ],
+      },
+    ],
+  },
+];
