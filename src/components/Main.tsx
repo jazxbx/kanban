2fedba0807ef29c80a3df794e9e8126b60899cf4
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-08-15 16:35:24 +0100

    added two shadcn components

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,15 +4,20 @@ import Sidebar from "./Sidebar";
 import defaultInfo from "@/lib/data";
 
 const [data, setData] = useState(defaultInfo);
+const [currentBoardIndex, setCurrentBoardIndex] = useState(undefined);
 
 export default function Main() {
   return (
     <>
       <div>
         {/* <Header data={data} /> */}
-        <Sidebar data={data} />
+        <Sidebar
+          data={data}
+          currentBoardIndex={currentBoardIndex}
+          setCurrentBoardIndex={setCurrentBoardIndex}
+        />
       </div>
-      <Board data={data} />
+      <Board data={data} currentBoardIndex={currentBoardIndex} />
     </>
   );
 }
