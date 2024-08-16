1146f6e3056e2ff017a4b3714273a4f4f7b3394a
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-08-16 15:24:35 +0100

    final commit

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -15,7 +15,7 @@ export default function TaskCard({ task }) {
     <Card className="h-[110px] w-[280px] flex flex-col shadow-lg dark:bg-gray-700">
       <CardHeader className="p-1 flex flex-row justify-between items-center">
         <TbArrowsMove className="m-3" />
-        <FaEllipsisVertical className="m-3" />
+        <FaEllipsisVertical className="m-3 mt-0" />
       </CardHeader>
       <CardContent className="text-wrap truncate font-bold text-base">
         {task.name}
