9ad6c09dcf78427842529ca86d337d55cb3c17f4
Ryan Wilson <ryanjoelwilson@gmail.com>
2024-08-16 12:01:39 +0100

    changed icon margins

--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -14,8 +14,8 @@ export default function TaskCard({ task }) {
   return (
     <Card className="h-[110px] w-[280px] flex flex-col shadow-lg dark:bg-gray-700">
       <CardHeader className="p-1 flex flex-row justify-between items-center">
-        <TbArrowsMove className="m-4" />
-        <FaEllipsisVertical className="m-4" />
+        <TbArrowsMove className="m-3" />
+        <FaEllipsisVertical className="m-3" />
       </CardHeader>
       <CardContent className="text-wrap truncate font-bold text-base">
         {task.name}
