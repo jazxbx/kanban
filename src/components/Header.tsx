5969ccd7592e4404a9a667c7abc04c423058c0e8
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-08-19 07:38:09 -0700

    Add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,25 @@
+import ThemeSwitch from './ThemeSwitch';
+import Image from 'next/image';
+import logoDark from '@/assets/logo-light.svg';
+import logoLight from '@/assets/logo-dark.svg';
+import logoMobile from '@/assets/logo-mobile.svg';
+
+export default function Header({ boardName }: { boardName: string }) {
+  return (
+    <div className='h-auto py-6 px-4 flex bg-white dark:bg-gray-600 justify-between items-center'>
+      <div className='flex'>
+        <div className='hidden md:inline-block mr-28'>
+          <Image className='block dark:hidden' src={logoLight} alt='logo' />
+          <Image className='hidden dark:block' src={logoDark} alt='logo' />
+        </div>
+        <div className='md:hidden'>
+          <Image src={logoMobile} alt='logo' />
+        </div>
+        <p className='text-lg font-bold md:text-2xl ml-4'>{boardName}</p>
+      </div>
+      <div>
+        <ThemeSwitch />
+      </div>
+    </div>
+  );
+}
