6eb56ab21936897376d44d8d469d8c73260a5851
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-08-21 21:52:35 -0700

    -add types to props -remove vertical scroll in column

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,28 +1,35 @@
-import { ScrollArea } from '@radix-ui/react-scroll-area';
-import { ScrollBar } from '@/components/ui/scroll-area';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
 import { useState } from 'react';
 import AddTask from './AddTask';
 import { Button } from './ui/button';
-import TaskCard from '../components/TaskCard';
 
-export default function Column({ column, data, setData, currentBoardIndex }) {
+import KanbanData from '@/lib/types';
+import TaskCard from './TaskCard';
+
+export default function Column({
+  column,
+  data,
+  setData,
+  currentBoardIndex,
+}: {
+  column: KanbanData['columns'][0];
+  data: KanbanData[];
+  setData: (data: KanbanData[]) => void;
+  currentBoardIndex: number;
+}) {
   const [addingTask, setAddingTask] = useState(false);
   return (
-    <Card className='min-w-[336px] h-fit shadow bg-gray-50 dark:bg-gray-600'>
+    <Card className='h-fit shadow bg-gray-50  dark:bg-gray-600 '>
       <CardHeader>
         <CardTitle className='text-ellipsis'>{column.name}</CardTitle>
       </CardHeader>
       <CardContent>
-        <ScrollArea className='min-h-[110px] min-w-[280px]'>
-          <div className='flex flex-col gap-3'>
-            {column.tasks.map((task) => (
-              <TaskCard task={task} key={task.id} />
-            ))}
-          </div>
-          <ScrollBar orientation='vertical' />
-        </ScrollArea>
+        <div className='flex flex-col gap-3'>
+          {column.tasks.map((task) => (
+            <TaskCard task={task} key={task.id} />
+          ))}
+        </div>
         {addingTask ? (
           <AddTask
             column={column}
