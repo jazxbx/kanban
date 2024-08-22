58db4e834d544428e29a6ee4bfce92f1648c8d73
Jazz <106460341+jazxbx@users.noreply.github.com>
2024-08-21 21:49:32 -0700

    add types to props

--- src/components/AddBoard.tsx
+++ src/components/AddBoard.tsx
@@ -1,9 +1,10 @@
-import { Button } from "./ui/button";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Input } from "./ui/input";
-import { createId } from "@/lib/createId";
+import { Button } from './ui/button';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from './ui/input';
+import { createId } from '@/lib/createId';
+import KanbanData from '@/lib/types';
 
 import {
   Form,
@@ -11,20 +12,28 @@ import {
   FormField,
   FormItem,
   FormMessage,
-} from "@/components/ui/form";
+} from '@/components/ui/form';
 
-export default function AddBoard({ data, setData, setAddingBoard }) {
+export default function AddBoard({
+  data,
+  setData,
+  setAddingBoard,
+}: {
+  data: KanbanData[];
+  setData: (data: KanbanData[]) => void;
+  setAddingBoard: (value: boolean) => void;
+}) {
   const formSchema = z.object({
     board: z
       .string()
       .min(1, {
         message: "Board name can't be blank",
       })
-      .max(20, { message: "Please shorten the board name" }),
+      .max(20, { message: 'Please shorten the board name' }),
   });
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: { board: "" },
+    defaultValues: { board: '' },
   });
   function onSubmit(values: z.infer<typeof formSchema>) {
     const { board } = values;
@@ -40,28 +49,28 @@ export default function AddBoard({ data, setData, setAddingBoard }) {
 
   return (
     <Form {...form}>
-      <form className="mt-4" onSubmit={form.handleSubmit(onSubmit)}>
+      <form className='mt-4' onSubmit={form.handleSubmit(onSubmit)}>
         <FormField
           control={form.control}
-          name="board"
+          name='board'
           render={({ field }) => (
             <FormItem>
               <FormControl>
-                <Input {...field} className="border-black dark:border-white" />
+                <Input {...field} className='border-black dark:border-white' />
               </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
-        <div className="mt-2">
-          <Button type="submit" variant="outline">
+        <div className='mt-2'>
+          <Button type='submit' variant='outline'>
             Add
           </Button>
           <Button
             onClick={handleCancel}
-            type="button"
-            variant="outline"
-            className="ml-1"
+            type='button'
+            variant='outline'
+            className='ml-1'
           >
             Cancel
           </Button>
